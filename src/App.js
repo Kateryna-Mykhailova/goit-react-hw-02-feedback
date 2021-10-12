1e017018253c582c7818ea743925651704738438
Kateryna-Mykhailova <katarrina.mk@gmail.com>
2021-10-12 12:23:56 +0200

    переустанавливает eslint, создает components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-
+import React from 'react';
 import './App.css';
+// import Feedback from ';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Feedback.css';
+// import styles from '../Feedback/Feedback.module.css''
+
+class Feedback extends React.Component {
+  render() {
+    return (
+      <div className="Feedback_container">
+        <button type="button">+</button>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
